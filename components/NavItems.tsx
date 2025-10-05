b9a5c848c31439dfacb15f561c332b1d87a5c34d
anujsamdariya07 <anujsamdariya07@gmail.com>
2025-10-05 10:59:29 +0530

    add trading view widget and it's custom hook; update home page to display market overview, heatmap, top stories, and market quotes;

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-'use client';
-
-import { NAV_ITEMS } from '@/lib/constants';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import React from 'react';
-
-const NavItems = () => {
-  const pathname = usePathname();
-
-  const isActive = (path: string) => {
-    if (path === '/') return pathname === '/';
-
-    return pathname.startsWith(path);
-  };
-
-=======
 'use client'
 
 import { NAV_ITEMS } from '@/lib/constants'
@@ -32,34 +14,17 @@ const NavItems = () => {
     return pathname.startsWith(path)
   }
   
->>>>>>> 92c5a0bf6d4db08dec68fdb76d13e1d3f4a89c47
   return (
     <ul className='flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium'>
       {NAV_ITEMS.map((item: NAV_ITEMS) => (
         <li key={item.href}>
-<<<<<<< HEAD
-          <Link
-            href={item.href}
-            className={`hover:text-yellow-500 transition-colors ${
-              isActive(item.href) ? 'text-gray-100' : ''
-            }`}
-          >
-=======
           <Link href={item.href} className={`hover:text-yellow-500 transition-colors ${isActive(item.href)? 'text-gray-100': ''}`}>
->>>>>>> 92c5a0bf6d4db08dec68fdb76d13e1d3f4a89c47
-            {item.title}
+            {item.label}
           </Link>
         </li>
       ))}
     </ul>
-<<<<<<< HEAD
-  );
-};
-
-export default NavItems;
-=======
   )
 }
 
 export default NavItems
->>>>>>> 92c5a0bf6d4db08dec68fdb76d13e1d3f4a89c47
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,11 +1,7 @@
 'use client';
 
 import React from 'react';
-<<<<<<< HEAD
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-=======
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
->>>>>>> 92c5a0bf6d4db08dec68fdb76d13e1d3f4a89c47
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -29,23 +25,15 @@ const UserDropdown = () => {
   const user = {
     name: 'John',
     email: 'contact@js.com',
-<<<<<<< HEAD
   };
-=======
-  }
->>>>>>> 92c5a0bf6d4db08dec68fdb76d13e1d3f4a89c47
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-<<<<<<< HEAD
         <Button
           variant={'ghost'}
           className='flex items-center gap-3 text-gray-400 hover:text-yellow-500'
         >
-=======
-        <Button variant={'ghost'} className='flex items-center gap-3 text-gray-400 hover:text-yellow-500'>
->>>>>>> 92c5a0bf6d4db08dec68fdb76d13e1d3f4a89c47
           <Avatar className='h-8 w-8'>
             {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
             <AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold'>
@@ -62,7 +50,6 @@ const UserDropdown = () => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className='text-gray-400'>
         <DropdownMenuLabel>
-<<<<<<< HEAD
           <div className='flex relative items-center gap-3 py-2'>
             <Avatar className='h-10 w-10'>
               {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
@@ -83,37 +70,12 @@ const UserDropdown = () => {
           onClick={handleSignOut}
           className='text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer'
         >
-=======
-        <div className='flex relative items-center gap-3 py-2'>
-          <Avatar className='h-10 w-10'>
-            {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
-            <AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold'>
-              {user.name[0]}
-            </AvatarFallback>
-          </Avatar>
-          <div className='flex flex-col'>
-            <span className='text-base font-medium text-gray-400'>
-              {user.name}
-            </span>
-            <span className='text-sm text-gray-500'>
-              {user.email}
-            </span>
-          </div>
-        </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator className='bg-gray-600' />
-        <DropdownMenuItem onClick={handleSignOut} className='text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer'>
->>>>>>> 92c5a0bf6d4db08dec68fdb76d13e1d3f4a89c47
           <LogOut className='h-4 w-4 mr-2 hidden sm:block' />
           Logout
         </DropdownMenuItem>
         <DropdownMenuSeparator className='hidden sm:block bg-gray-600' />
         <nav className='sm:hidden'>
-<<<<<<< HEAD
           <NavItems />
-=======
-          <NavItems/>
->>>>>>> 92c5a0bf6d4db08dec68fdb76d13e1d3f4a89c47
         </nav>
       </DropdownMenuContent>
     </DropdownMenu>
